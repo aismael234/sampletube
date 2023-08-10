80158f48ceb0b9e73db57661f1275511519153d6
aismael234 <ismael.aladdin@gmail.com>
2023-08-09 17:29:07 -0700

    Minor responsive improvements

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -31,60 +31,54 @@ export default function Filter(props) {
         Filter
       </h4>
       <div className="inputs-container">
-        <div className="input-row">
-          <label htmlFor="minimum_views">Minimum Views</label>
+        <label htmlFor="minimum_views">Minimum Views</label>
+        <input
+          id="minimum_views"
+          name="minimum_views"
+          placeholder="1000"
+          min="0"
+          type="number"
+          value={props.form.minimum_views}
+          onChange={props.handleForm}
+        />
+        <label htmlFor="maximum_views">Maximum Views</label>
+        <input
+          id="maximum_views"
+          name="maximum_views"
+          placeholder="300000"
+          min="0"
+          type="number"
+          value={props.form.maximum_views}
+          onChange={props.handleForm}
+        />
+        <label htmlFor="date_from">Uploaded between </label>
+        <div className="dates-container">
           <input
-            id="minimum_views"
-            name="minimum_views"
-            placeholder="1000"
-            min="0"
-            type="number"
-            value={props.form.minimum_views}
-            onChange={props.handleForm}
-          />
-        </div>
-        <div className="input-row">
-          <label htmlFor="maximum_views">Maximum Views</label>
-          <input
-            id="maximum_views"
-            name="maximum_views"
-            placeholder="300000"
-            min="0"
-            type="number"
-            value={props.form.maximum_views}
-            onChange={props.handleForm}
-          />
-        </div>
-        <div id="input-date" className="input-row">
-          <label htmlFor="date_from">Uploaded between </label>
-          <input
-            style={{ width: "60px" }}
             type="date"
             name="date_from"
             id="date_from"
+            placeholder="MM/DD/YYYY"
             value={props.form.date_from}
             onChange={props.handleForm}
           />
           <label htmlFor="date_to">and</label>
           <input
-            style={{ width: "60px" }}
             type="date"
             name="date_to"
             id="date_to"
+            placeholder="MM/DD/YYYY"
             value={props.form.date_to}
             onChange={props.handleForm}
           />
         </div>
-        <div className="input-row" id="input-keywords">
-          <label htmlFor="keywords">Keywords </label>
-          <textarea
-            style={{ width: "150px" }}
-            name="keywords"
-            id="keywords"
-            value={props.form.keywords}
-            onChange={props.handleForm}
-          />
-        </div>
+        <label htmlFor="keywords">Keywords </label>
+        <textarea
+          name="keywords"
+          id="keywords"
+          placeholder="Orchestra"
+          value={props.form.keywords}
+          onChange={props.handleForm}
+        />
         <div className="buttons-container">
           <button
             type="reset"
